a3f1531bf18c129aa099a830c122941b31e84613
xmartinly <778567144@qq.com>
2024-12-05 13:21:41 +0800

    %USERDOMAIN%\%username%  %date:~0,4%%date:~5,2%%date:~8,2%.%time:~0,2%%time:~3,2%%time:~6,2%

--- calc_tf.cpp
+++ calc_tf.cpp
@@ -8,6 +8,10 @@ CalcTF::CalcTF(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::CalcTF) {
     ui->setupUi(this);
+    ui->lb_thck->setHidden(!calc_freq_);
+    ui->le_thck->setHidden(!calc_freq_);
+    ui->lb_freq_range->setHidden(!calc_freq_);
+    ui->cb_freq_range->setHidden(!calc_freq_);
 }
 
 CalcTF::~CalcTF() {
@@ -15,29 +19,90 @@ CalcTF::~CalcTF() {
 }
 
 void CalcTF::on_btn_calc_clicked() {
-    // xtal stop frequency
-    double f_c = ui->le_freq_stop->text().toDouble();
+    calcFunction();
+}
+
+
+double CalcTF::deriCalcThickness(double f_c, double d_m, double matl_z, double f_q) const {
+    double term1 = (n_q * d_q / (pi * d_m * f_c * f_c * matl_z)) * atan(matl_z * tan(pi * (f_q - f_c) / f_q));
+    double term2 = (n_q * d_q / (pi * d_m * f_c * matl_z)) * matl_z * (1 / cos(pi * (f_q - f_c) / f_q)) * (-pi / f_q);
+    return term1 + term2;
+}
+
+double CalcTF::calcThickness(double f_c, double d_m, double matl_z, double f_q, double thck) const {
+    return (n_q * d_q / (pi * d_m * f_c * matl_z)) * atan(matl_z * tan(pi * (f_q - f_c) / f_q)) - thck;
+}
+
+double CalcTF::newtonRaphson(double initial_guess, double d_m,  double matl_z, double f_q, double thck, double epsilon) const {
+    double F_c = initial_guess;
+    double f_F_c = calcThickness(F_c, d_m, matl_z, f_q, thck);
+    double df_F_c = deriCalcThickness(F_c, d_m, matl_z, f_q);
+    while (fabs(f_F_c) > epsilon) {
+        F_c = F_c - f_F_c / df_F_c;
+        f_F_c = calcThickness(F_c, d_m, matl_z, f_q, thck);
+        df_F_c = deriCalcThickness(F_c, d_m, matl_z, f_q);
+    }
+    return F_c;
+}
+
+void CalcTF::calcFunction() const {
     // xtal start frequency
     double f_q = ui->le_freq_start->text().toDouble();
     // material z
     double matl_z = ui->le_matl_z->text().toDouble();
     // material density
     double matl_d = ui->le_matl_d->text().toDouble();
-    // time span
-    double acq_intvl = ui->le_meas_itl->text().toDouble();
-    if(!f_c || !f_q || !matl_d || !matl_z || !acq_intvl) {
+    if(calc_freq_) {
+        // input thickness
+        double thck = ui->le_thck->text().toDouble();
+        if(thck < 10) {
+            QMessageBox::information(nullptr, u8"厚度参数", u8"厚度参数错误，需要大于10的值");
+            return;
+        }
+        // calc frequency stop
+        double f_c_calc = newtonRaphson(5000000, matl_d, matl_z, f_q, thck);
+        ui->le_freq_stop->setText(QString::number(f_c_calc, 'f', 3));
+        double life = (f_q - f_c_calc ) / cyg_range_ * 100;
+        QString freq_text = u8"频率: \t" +  QString::number(f_c_calc, 'f', 3) + u8"\tHz";
+        QString life_text = u8"Life: \t" +  QString::number(life, 'f', 3) + u8"\t\t%";
+        ui->lbl_result->setText(freq_text + "\n" + life_text);
         return;
     }
-    ui->lbl_result->clear();
-    // part1 for normal calculate
-    double val_frnt = (n_q * d_q) / (pi * matl_d * f_c * matl_z);
-    // part2 for atan calculate
-    double val_back = atan(matl_z * tan(pi * ((f_q - f_c) / f_q)));
-    // thickness value
-    double thickness = val_frnt * val_back;
+    // time span
+    double acq_intvl = ui->le_meas_itl->text().toDouble();
+    // xtal stop frequency
+    double f_c = ui->le_freq_stop->text().toDouble();
+    if(f_c == f_q || f_q < f_c) {
+        QMessageBox::information(nullptr, u8"频率", u8"频率输入错误。");
+        return;
+    }
+    if(acq_intvl < 10) {
+        QMessageBox::information(nullptr, u8"间隔时间", u8"间隔时间过短，需要设置为10ms以上。");
+        return;
+    }
+    double thck = calcThickness(f_c, matl_d, matl_z, f_q);
     // thickness text, include unit
-    QString text_thickness = u8"厚度: \t" +  QString::number(thickness, 'f', 3) + u8"\tÅ";
+    QString text_thickness = u8"厚度: \t" +  QString::number(thck, 'f', 3) + u8"\tÅ";
     // rate text, include unit
-    QString text_rate = u8"速率: \t" + QString::number(thickness * 1000 / acq_intvl, 'f', 3) + u8"\tÅ/s";
+    QString text_rate = u8"速率: \t" + QString::number(thck * 1000 / acq_intvl, 'f', 3) + u8"\tÅ/s";
     ui->lbl_result->setText(text_thickness + "\n" + text_rate);
 }
+
+
+void CalcTF::on_cb_method_currentIndexChanged(int index) {
+    calc_freq_ = index;
+    ui->le_freq_stop->setHidden(calc_freq_);
+    ui->le_meas_itl->setHidden(calc_freq_);
+    ui->lb_f_c->setHidden(calc_freq_);
+    ui->lb_meas_itl->setHidden(calc_freq_);
+    ui->lb_thck->setHidden(!calc_freq_);
+    ui->le_thck->setHidden(!calc_freq_);
+    ui->lb_freq_range->setHidden(!calc_freq_);
+    ui->cb_freq_range->setHidden(!calc_freq_);
+}
+
+
+void CalcTF::on_cb_freq_range_currentIndexChanged(int index) {
+    cyg_range_ = index ? 1000000 : 1500000;
+}
+
--- calc_tf.h
+++ calc_tf.h
@@ -3,6 +3,7 @@
 #define CALCTF_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 
 
 QT_BEGIN_NAMESPACE
@@ -23,16 +24,30 @@ class CalcTF : public QMainWindow
   private slots:
     void on_btn_calc_clicked();
 
+    // void on_btn_calc_rev_clicked();
+
+    void on_cb_method_currentIndexChanged(int index);
+
+    void on_cb_freq_range_currentIndexChanged(int index);
+
   private:
     Ui::CalcTF* ui;
 
-    void set_res_text();
     // pi value
     const double pi = 3.1415926;
     // xtal density
     const double d_q = 2.648;
     // xtal constant
     const double n_q = 1.668e13;
+    //ic6 cygnus2 frequency range
+    double cyg_range_ = 1500000;
+    bool calc_freq_ = false;
+    double deriCalcThickness(double f_c, double d_m, double matl_z, double f_q)const;
+    double calcThickness(double f_c, double d_m, double matl_z, double f_q, double thck = 0)const;
+    double newtonRaphson(double initial_guess, double d_m,  double matl_z, double f_q, double thck, double epsilon = 1e-6) const;
+
+    void calcFunction() const;
+
 
 
 };
